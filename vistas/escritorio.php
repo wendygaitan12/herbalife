d8a40f6229b0cbde19c00a7d1756d1a6f3d1732e
wendygaitan12 <54187241+wendygaitan12@users.noreply.github.com>
2019-09-21 15:38:37 -0600

    Update escritorio.php

--- vistas/escritorio.php
+++ vistas/escritorio.php
@@ -13,7 +13,86 @@ require 'header.php';
 
 if ($_SESSION['escritorio']==1)
 {
- 
+
+}
+
+
+?>
+<!--Contenido-->
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">        
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="box">
+                    <div class="box-header with-border">
+                          <h1 class="box-title">Escritorio </h1>
+                        <div class="box-tools pull-right">
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- centro -->
+                    <div class="panel-body">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <div class="small-box bg-aqua">
+                              <div class="inner">
+                                <h4 style="font-size:17px;">
+                                  <strong>S/ <?php echo $totalc; ?></strong>
+                                </h4>
+                                <p>Compras</p>
+                              </div>
+                              <div class="icon">
+                                <i class="ion ion-bag"></i>
+                              </div>
+                              <a href="ingreso.php" class="small-box-footer">Compras <i class="fa fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <div class="small-box bg-green">
+                              <div class="inner">
+                                <h4 style="font-size:17px;">
+                                  <strong>S/ <?php echo $totalv; ?></strong>
+                                </h4>
+                                <p>Ventas</p>
+                              </div>
+                              <div class="icon">
+                                <i class="ion ion-bag"></i>
+                              </div>
+                              <a href="venta.php" class="small-box-footer">Ventas <i class="fa fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <div class="box box-primary">
+                              <div class="box-header with-border">
+                                Compras de los últimos 10 días
+                              </div>
+                              <div class="box-body">
+                                <canvas id="compras" width="400" height="300"></canvas>
+                              </div>
+                          </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <div class="box box-primary">
+                              <div class="box-header with-border">
+                                Ventas de los últimos 12 meses
+                              </div>
+                              <div class="box-body">
+                                <canvas id="ventas" width="400" height="300"></canvas>
+                              </div>
+                          </div>
+                        </div>
+                    </div>
+                    <!--Fin centro -->
+                  </div><!-- /.box -->
+              </div><!-- /.col -->
+          </div><!-- /.row -->
+      </section><!-- /.content -->
+
+    </div><!-- /.content-wrapper -->
+  <!--Fin-Contenido-->
 <?php
 }
 else
@@ -128,6 +207,8 @@ var ventas = new Chart(ctx, {
 }
 ob_end_flush();
 ?>
+
+
 
 
 

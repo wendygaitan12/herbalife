402735743874057834d8ad8cc159148ca842f384
wendygaitan12 <54187241+wendygaitan12@users.noreply.github.com>
2019-09-21 17:03:46 -0600

    Update escritorio.php

--- vistas/escritorio.php
+++ vistas/escritorio.php
@@ -27,10 +27,9 @@ require 'header.php';
                     <div class="panel-body">
 
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                          <div class="small-box bg-aqua">
+                          <div class="small-box bg-yellow">
                               <div class="inner">
                                 <h4 style="font-size:17px;">
-                                  <strong>Q <?php echo $totalc; ?></strong>
                                 </h4>
                                 <p>Categoria</p>
                               </div>
@@ -40,12 +39,25 @@ require 'header.php';
                               <a href="categoria.php" class="small-box-footer">Categoria <i class="fa fa-arrow-circle-right"></i></a>
                             </div>
                         </div>
+
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <div class="small-box bg-Red">
+                              <div class="inner">
+                                <h4 style="font-size:17px;">
+                                </h4>
+                                <p>Producto</p>
+                              </div>
+                              <div class="icon">
+                                <i class="ion ion-bag"></i>
+                              </div>
+                              <a href="categoria.php" class="small-box-footer">Producto <i class="fa fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
                        
                         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
                           <div class="small-box bg-aqua">
                               <div class="inner">
                                 <h4 style="font-size:17px;">
-                                  <strong>Q <?php echo $totalc; ?></strong>
                                 </h4>
                                 <p>Compras</p>
                               </div>
@@ -60,7 +72,6 @@ require 'header.php';
                           <div class="small-box bg-green">
                               <div class="inner">
                                 <h4 style="font-size:17px;">
-                                  <strong>Q <?php echo $totalv; ?></strong>
                                 </h4>
                                 <p>Ventas</p>
                               </div>
@@ -70,6 +81,21 @@ require 'header.php';
                               <a href="venta.php" class="small-box-footer">Ventas <i class="fa fa-arrow-circle-right"></i></a>
                             </div>
                         </div>
+
+                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <div class="small-box bg-Blue">
+                              <div class="inner">
+                                <h4 style="font-size:17px;">
+                                </h4>
+                                <p>Usuario</p>
+                              </div>
+                              <div class="icon">
+                                <i class="ion ion-bag"></i>
+                              </div>
+                              <a href="categoria.php" class="small-box-footer">Usuario <i class="fa fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                       
 
                     </div>
             
@@ -88,5 +114,3 @@ require 'footer.php';
 }
 ob_end_flush();
 ?>
-
-

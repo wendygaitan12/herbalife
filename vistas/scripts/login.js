87505f5c1870ec4c59fbf3519672e03f58a468c6
wendygaitan12 <54187241+wendygaitan12@users.noreply.github.com>
2019-09-21 16:26:32 -0600

    cambio de escritorio a articulo cuando logea
    cambio de escritorio a articulo cuando logea

--- vistas/scripts/login.js
+++ vistas/scripts/login.js
@@ -10,7 +10,7 @@ $("#frmAcceso").on('submit',function(e)
     {
         if (data!="null")
         {
-            $(location).attr("href","escritorio.php");            
+            $(location).attr("href","articulo.php");            
         }
         else
         {

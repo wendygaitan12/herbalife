a42a328eba322af7e4355992d18f776d607d2fb1
wendygaitan12 <54187241+wendygaitan12@users.noreply.github.com>
2019-09-21 14:38:02 -0600

    Carga de la carpeta de escripts
    Carga de la carpeta de escripts

---
+++ vistas/scripts/usuario.js
@@ -0,0 +1,202 @@
+var tabla;
+
+//Función que se ejecuta al inicio
+function init(){
+	mostrarform(false);
+	listar();
+
+	$("#formulario").on("submit",function(e)
+	{
+		guardaryeditar(e);	
+	})
+
+	$("#imagenmuestra").hide();
+	//Mostramos los permisos
+	$.post("../ajax/usuario.php?op=permisos&id=",function(r){
+	        $("#permisos").html(r);
+	});
+
+	//Referencia del usuario 
+
+	$.post("../ajax/usuario.php?op=SelectUsuarioReferencia", function(r){
+	            $("#idreferencia").html(r);
+	            $('#idreferencia').selectpicker('refresh');
+
+	});
+	$('#mAcceso').addClass("treeview active");
+    $('#lUsuarios').addClass("active");
+}
+
+//Función limpiar
+function limpiar()
+{
+	$("#nombre").val("");
+	$("#num_documento").val("");
+	$("#direccion").val("");
+	$("#telefono").val("");
+	$("#email").val("");
+	$("#cargo").val("");
+	$("#login").val("");
+	//puntos y refencia
+	$("#puntos").val("");
+	$("#idreferencia").val("");
+	$("#clave").val("");
+	$("#imagenmuestra").attr("src","");
+	$("#imagenactual").val("");
+	$("#idusuario").val("");
+}
+
+//Función mostrar formulario
+function mostrarform(flag)
+{
+	limpiar();
+	if (flag)
+	{
+		$("#listadoregistros").hide();
+		$("#formularioregistros").show();
+		$("#btnGuardar").prop("disabled",false);
+		$("#btnagregar").hide();
+	}
+	else
+	{
+		$("#listadoregistros").show();
+		$("#formularioregistros").hide();
+		$("#btnagregar").show();
+	}
+}
+
+//Función cancelarform
+function cancelarform()
+{
+	limpiar();
+	mostrarform(false);
+}
+
+//Función Listar
+function listar()
+{
+	tabla=$('#tbllistado').dataTable(
+	{
+		"lengthMenu": [ 5, 10, 25, 75, 100],//mostramos el menú de registros a revisar
+		"aProcessing": true,//Activamos el procesamiento del datatables
+	    "aServerSide": true,//Paginación y filtrado realizados por el servidor
+	    dom: '<Bl<f>rtip>',//Definimos los elementos del control de tabla
+	    buttons: [		          
+		            'copyHtml5',
+		            'excelHtml5',
+		            'csvHtml5',
+		            'pdf'
+		        ],
+		"ajax":
+				{
+					url: '../ajax/usuario.php?op=listar',
+					type : "get",
+					dataType : "json",						
+					error: function(e){
+						console.log(e.responseText);	
+					}
+				},
+		"language": {
+            "lengthMenu": "Mostrar : _MENU_ registros",
+            "buttons": {
+            "copyTitle": "Tabla Copiada",
+            "copySuccess": {
+                    _: '%d líneas copiadas',
+                    1: '1 línea copiada'
+                }
+            }
+        },
+		"bDestroy": true,
+		"iDisplayLength": 5,//Paginación
+	    "order": [[ 0, "desc" ]]//Ordenar (columna,orden)
+	}).DataTable();
+}
+//Función para guardar o editar
+
+function guardaryeditar(e)
+{
+	e.preventDefault(); //No se activará la acción predeterminada del evento
+	$("#btnGuardar").prop("disabled",true);
+	var formData = new FormData($("#formulario")[0]);
+
+	$.ajax({
+		url: "../ajax/usuario.php?op=guardaryeditar",
+	    type: "POST",
+	    data: formData,
+	    contentType: false,
+	    processData: false,
+
+	    success: function(datos)
+	    {                    
+	          bootbox.alert(datos);	          
+	          mostrarform(false);
+	          tabla.ajax.reload();
+	    }
+
+	});
+	limpiar();
+}
+
+//funcion mostrar dt
+function mostrar(idusuario)
+{
+	$.post("../ajax/usuario.php?op=mostrar",{idusuario : idusuario}, function(data, status)
+	{
+		data = JSON.parse(data);		
+		mostrarform(true);
+
+		$("#nombre").val(data.nombre);
+		$("#tipo_documento").val(data.tipo_documento);
+		$("#tipo_documento").selectpicker('refresh');
+		$("#num_documento").val(data.num_documento);
+		$("#direccion").val(data.direccion);
+		$("#telefono").val(data.telefono);
+		$("#email").val(data.email);
+		$("#cargo").val(data.cargo);
+		
+		//puntos y referencia 
+		$("#puntos").val(data.puntos);
+		$("#idreferencia").val(data.idreferencia);
+		$("#idreferencia").selectpicker('refresh');
+		$("#login").val(data.login);
+		$("#clave").val(data.clave);
+		$("#imagenmuestra").show();
+		$("#imagenmuestra").attr("src","../files/usuarios/"+data.imagen);
+		$("#imagenactual").val(data.imagen);
+		$("#idusuario").val(data.idusuario);
+
+ 	});
+ 	$.post("../ajax/usuario.php?op=permisos&id="+idusuario,function(r){
+	        $("#permisos").html(r);
+	});
+}
+
+//Función para desactivar registros
+function desactivar(idusuario)
+{
+	bootbox.confirm("¿Está Seguro de desactivar el usuario?", function(result){
+		if(result)
+        {
+        	$.post("../ajax/usuario.php?op=desactivar", {idusuario : idusuario}, function(e){
+        		bootbox.alert(e);
+	            tabla.ajax.reload();
+        	});	
+        }
+	})
+}
+
+//Función para activar registros
+function activar(idusuario)
+{
+	bootbox.confirm("¿Está Seguro de activar el Usuario?", function(result){
+		if(result)
+        {
+        	$.post("../ajax/usuario.php?op=activar", {idusuario : idusuario}, function(e){
+        		bootbox.alert(e);
+	            tabla.ajax.reload();
+        	});	
+        }
+	})
+}
+
+init();

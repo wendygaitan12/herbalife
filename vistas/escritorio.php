b45df2de7015c0fa7d463e30cd4a16c1a5208459
wendygaitan12 <54187241+wendygaitan12@users.noreply.github.com>
2019-09-21 16:47:27 -0600

    Update escritorio.php

--- vistas/escritorio.php
+++ vistas/escritorio.php
@@ -11,25 +11,70 @@ else
 {
 require 'header.php';
 ?>
-<!--Contenido-->
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">        
+ <div class="content-wrapper">        
         <!-- Main content -->
         <section class="content">
             <div class="row">
               <div class="col-md-12">
                   <div class="box">
                     <div class="box-header with-border">
-                          <h1 class="box-title">Información del Sistema </h1>
-	                        <div class="box-tools pull-right">
-	                        </div>
+                          <h1 class="box-title">Escritorio </h1>
+                        <div class="box-tools pull-right">
+                        </div>
                     </div>
                     <!-- /.box-header -->
                     <!-- centro -->
                     <div class="panel-body">
-                    <h4>Proyecto: </h4> <p>Sistema de Administración de Productos Herbalife </p>
-		                <h4>Empresa: </h4> <p>Universidad Mariano Gálvez, Facultad de Ingeniería en Sistemas </p>
-		                <h4>Desarrollado por: </h4> <p>Wendy Gaitán </p>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <div class="small-box bg-aqua">
+                              <div class="inner">
+                                <h4 style="font-size:17px;">
+                                  <strong>Q <?php echo $totalc; ?></strong>
+                                </h4>
+                                <p>Compras</p>
+                              </div>
+                              <div class="icon">
+                                <i class="ion ion-bag"></i>
+                              </div>
+                              <a href="ingreso.php" class="small-box-footer">Compras <i class="fa fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <div class="small-box bg-green">
+                              <div class="inner">
+                                <h4 style="font-size:17px;">
+                                  <strong>Q <?php echo $totalv; ?></strong>
+                                </h4>
+                                <p>Ventas</p>
+                              </div>
+                              <div class="icon">
+                                <i class="ion ion-bag"></i>
+                              </div>
+                              <a href="venta.php" class="small-box-footer">Ventas <i class="fa fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <div class="box box-primary">
+                              <div class="box-header with-border">
+                                Compras de los últimos 10 días
+                              </div>
+                              <div class="box-body">
+                                <canvas id="compras" width="400" height="300"></canvas>
+                              </div>
+                          </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                          <div class="box box-primary">
+                              <div class="box-header with-border">
+                                Ventas de los últimos 12 meses
+                              </div>
+                              <div class="box-body">
+                                <canvas id="ventas" width="400" height="300"></canvas>
+                              </div>
+                          </div>
+                        </div>
                     </div>
                     <!--Fin centro -->
                   </div><!-- /.box -->
